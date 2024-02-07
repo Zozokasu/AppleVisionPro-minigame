689fee2307f6ec691f45433aa0794307f8e0e5bd
rassi0429 <taitokokoa+rassi@gmail.com>
2024-02-07 12:56:04 +0900

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by こばとここあ on 2024/02/07.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
